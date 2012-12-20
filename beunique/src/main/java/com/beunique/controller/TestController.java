cbf491b9aef17a422cca9a3fa8009f401a0dc2d6
nikolacakarevic <nikola.cakarevic@gmail.com>
2012-12-20 10:45:12 +0100

    Git test

---
+++ beunique/src/main/java/com/beunique/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.beunique.controller;
+
+public class TestController {
+
+	private String git;
+
+	public String getGit() {
+		return git;
+	}
+
+	public void setGit(String git) {
+		this.git = git;
+	}
+	
+	
+}
