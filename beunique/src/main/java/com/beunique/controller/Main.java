c0e3bbb5c0ea45d43b18452be6a0d353b2ecaf37
nikolacakarevic <nikola.cakarevic@gmail.com>
2012-12-20 11:35:13 +0100

    commit2

---
+++ beunique/src/main/java/com/beunique/controller/Main.java
@@ -0,0 +1,22 @@
+package com.beunique.controller;
+
+import java.io.File;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File f = new File("src/main/webapp/WEB-INF/applicationContext.xml");
+		System.out.println(f.getAbsolutePath());
+		ApplicationContext context = new ClassPathXmlApplicationContext("webapp/WEB-INF/applicationContext.xml");
+		TestController controller = (TestController) context.getBean("testController");
+		
+		System.out.println(controller.getGit());
+	}
+
+}
